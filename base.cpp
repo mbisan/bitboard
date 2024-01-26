b73aa8e8d86a3a8c05ff3962e0d7129505cd604c
mbisan <mbikandi@bcamath.org>
2024-01-26 15:08:30 +0100

    update logic

--- base.cpp
+++ base.cpp
@@ -26,6 +26,11 @@ Squares wRrookStart = 0x0000000000000080ULL;
 Squares bLrookStart = 0x0100000000000000ULL;
 Squares bRrookStart = 0x8000000000000000ULL;
 
+Squares wLCastleSeen = 0x000000000000000eULL;
+Squares wRCastleSeen = 0x0000000000000060ULL;
+Squares bLCastleSeen = 0x0e00000000000000ULL;
+Squares bRCastleSeen = 0x6000000000000000ULL;
+
 const struct GameState {
     const bool ep;
     const bool wL;
@@ -138,7 +143,41 @@ struct Board {
     template<GameState state>
     Board pawnPush(Squares pawnSquare, Squares move) {
         if constexpr (state.isWhite) return {w.movePawn(move), b, pawnSquare, state.pawnPush()};
-        else 
+        else return {w, b.movePawn(move), pawnSquare, state.pawnPush()}
+    }
+
+    template<GameState state, int piece>
+    Board pawnPromote(Squares pawnSquare, Squares move) {
+        if constexpr (state.isWhite) {
+            Pieces wn = w.movePawn(pawnSquare);
+            if constexpr (piece == QUEEN) return {wn.moveQueen(move), b, 0, state.move()};
+            if constexpr (piece == BISHOP) return {wn.moveBishop(move), b, 0, state.move()};
+            if constexpr (piece == KNIGHT) return {wn.moveKnight(move), b, 0, state.move()};
+            if constexpr (piece == ROOK) return {wn.movePawn(move), b, 0, state.move()};
+        } else {
+            Pieces bn = b.movePawn(pawnSquare);
+            if constexpr (piece == QUEEN) return {w, bn.moveQueen(move), 0, state.move()};
+            if constexpr (piece == BISHOP) return {w, bn.moveBishop(move), 0, state.move()};
+            if constexpr (piece == KNIGHT) return {w, bn.moveKnight(move), 0, state.move()};
+            if constexpr (piece == ROOK) return {w, bn.movePawn(move), 0, state.move()};
+        }
+    }
+
+    template<GameState state, int piece>
+    Board pawnPromoteCapture(Squares pawnSquare, Squares move) {
+        if constexpr (state.isWhite) {
+            Pieces wn = w.movePawn(pawnSquare);
+            if constexpr (piece == QUEEN) return {wn.moveQueen(move), b.remove(move), 0, state.move()};
+            if constexpr (piece == BISHOP) return {wn.moveBishop(move), b.remove(move), 0, state.move()};
+            if constexpr (piece == KNIGHT) return {wn.moveKnight(move), b.remove(move), 0, state.move()};
+            if constexpr (piece == ROOK) return {wn.movePawn(move), b.remove(move), 0, state.move()};
+        } else {
+            Pieces bn = b.movePawn(pawnSquare);
+            if constexpr (piece == QUEEN) return {w.remove(move), bn.moveQueen(move), 0, state.move()};
+            if constexpr (piece == BISHOP) return {w.remove(move), bn.moveBishop(move), 0, state.move()};
+            if constexpr (piece == KNIGHT) return {w.remove(move), bn.moveKnight(move), 0, state.move()};
+            if constexpr (piece == ROOK) return {w.remove(move), bn.movePawn(move), 0, state.move()};
+        }
     }
 
     template<GameState state, int piece>
@@ -172,6 +211,28 @@ struct Board {
                     if (move & bRrookStart) return {w.remove(notmove), b.moveRook(move), 0, state.RRookMove()};
                 }
             }
+        }
+    }
+
+    template<GameState state>
+    Board castleL() {
+        if constexpr (state.isWhite) {
+            Pieces wn = w.moveKing(0x0000000000000014ULL);
+            return {wn.moveRook(0x0000000000000009ULL), b, 0, state.kingMove()};
+        } else {
+            Pieces bn = b.moveKing(0x1400000000000000ULL);
+            return {wn.moveRook(0x0900000000000000ULL), b, 0, state.kingMove()};
+        }
+    }
+
+    template<GameState state>
+    Board castleR() {
+        if constexpr (state.isWhite) {
+            Pieces wn = w.moveKing(0x0000000000000050ULL);
+            return {wn.moveRook(0x00000000000000a0ULL), b, 0, state.kingMove()};
+        } else {
+            Pieces bn = b.moveKing(0x5000000000000000ULL);
+            return {wn.moveRook(0xa000000000000000ULL), b, 0, state.kingMove()};
         }
     }
 };
--- bitboard.cpp
+++ bitboard.cpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cstdint>
+#include <vector>
 
 struct statusReport {
     uint64_t checkCount;
@@ -17,7 +18,9 @@ struct statusReport {
     Squares epPin;
 };
 
-template<GameState state>
+#define BitLoop(reachable) for (uint64_t temp=reachable; temp; temp=_blsr_u64(temp))
+
+template<bool isWhite>
 statusReport check(const Pieces &self, const Pieces &enemy) {
     Squares selfOcc = self.occupied();
     Squares enemyOcc = enemy.occupied();
@@ -39,7 +42,7 @@ statusReport check(const Pieces &self, const Pieces &enemy) {
     for (Squares temp = enemy.r | enemy.q; temp; temp = _blsr_u64(temp)) {
         uint64_t pieceIndex = _tzcnt_u64(temp);
         Squares atk = rookMoves[pieceIndex];
-        Squares rookSeen = rookSlide(atk, occ, pieceIndex);
+        Squares rookSeen = slide(atk, occ, pieceIndex);
         enemySeen |= rookSeen;
 
         Squares pin = PinBetween[kingIndex][pieceIndex];
@@ -65,7 +68,7 @@ statusReport check(const Pieces &self, const Pieces &enemy) {
     for (Squares temp = enemy.b | enemy.q; temp; temp = _blsr_u64(temp)) {
         uint64_t pieceIndex = _tzcnt_u64(temp);
         Squares atk = bishopMoves[pieceIndex];
-        Squares bishopSeen = rookSlide(atk, occ, pieceIndex);
+        Squares bishopSeen = slide(atk, occ, pieceIndex);
         enemySeen |= bishopSeen;
 
         Squares pin = PinBetween[kingIndex][pieceIndex];
@@ -96,7 +99,7 @@ statusReport check(const Pieces &self, const Pieces &enemy) {
     // pawns
     Squares atkL;
     Squares atkR;
-    if constexpr (state.isWhite) {
+    if constexpr (isWhite) {
         atkL = (enemy.p & notAfile) >> 7;
         atkR = (enemy.p & notHfile) >> 9;
         if (atkL & self.k) checkMask |= (self.k << 7) & enemy.p;
@@ -111,4 +114,308 @@ statusReport check(const Pieces &self, const Pieces &enemy) {
     enemySeen |= kingMoves[bitFromSquare(enemy.k)];
 
     return {checkCount, kingIndex, checkMask, kingBan, pinHV, pinD, enemySeen, selfOcc, enemyOcc, epPin};
+}
+
+template<GameState state>
+std::vector<Board> generateMoves(const Board &board) {
+    Pieces self, enemy;
+    if constexpr (state.isWhite) {
+        self = board.w; enemy = board.b;
+    } else {
+        self = board.b; enemy = board.w;
+    }
+
+    std::vector<Board> out;
+
+    statusReport res = check<state.isWhite>(self, enemy);
+    Squares notEnemy = ~res.enemyOcc;
+    Squares notSelf = ~res.selfOcc;
+    Squares occ = res.selfOcc | res.enemyOcc;
+    Squares notpin = ~(res.pinHV | res.pinD);
+
+    // king moves
+    {    
+        Squares kingReachable = kingMoves[res.kingIndex] & ~res.kingBan & ~res.enemySeen & notSelf;
+        BitLoop(kingReachable & notEnemy) { out.push_back(board.pieceMove<state, KING>(_blsi_u64(temp) | self.k)); }
+        BitLoop(kingReachable & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, KING>(_blsi_u64(temp) | self.k)); }
+
+        if (res.checkCount > 1) { // only king can move
+            return out;
+        }
+    }
+
+    Squares notselfCheckmask = notSelf & res.checkMask;
+
+    // rook moves
+    {
+        // pinned
+        for (Squares pinnedRooks = self.r & res.pinHV; pinnedRooks; pinnedRooks = _blsr_u64(pinnedRooks)) {
+            Squares current = _blsi_u64(pinnedRooks);
+            uint64_t pieceIndex = _tzcnt_u64(pinnedRooks);
+            Squares atk = slide(rookMoves[pieceIndex], occ, pieceIndex) & notselfCheckmask & res.pinHV;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, ROOK>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, ROOK>(_blsi_u64(temp) | current)); }
+        }
+        // not pinned
+        for (Squares unpinnedRooks = self.r & notpin; unpinnedRooks; unpinnedRooks = _blsr_u64(unpinnedRooks)) {
+            Squares current = _blsi_u64(unpinnedRooks);
+            uint64_t pieceIndex = _tzcnt_u64(unpinnedRooks);
+            Squares atk = slide(rookMoves[pieceIndex], occ, pieceIndex) & notselfCheckmask;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, ROOK>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, ROOK>(_blsi_u64(temp) | current)); }
+        }
+    }
+
+    // bishop moves
+    {
+        // pinned
+        for (Squares pinnedBishops = self.r & res.pinD; pinnedBishops; pinnedBishops = _blsr_u64(pinnedBishops)) {
+            Squares current = _blsi_u64(pinnedBishops);
+            uint64_t pieceIndex = _tzcnt_u64(pinnedBishops);
+            Squares atk = slide(bishopMoves[pieceIndex], occ, pieceIndex) & notselfCheckmask & res.pinD;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, BISHOP>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, BISHOP>(_blsi_u64(temp) | current)); }
+        }
+        // not pinned
+        for (Squares unpinnedBishops = self.r & notpin; unpinnedBishops; unpinnedBishops = _blsr_u64(unpinnedBishops)) {
+            Squares current = _blsi_u64(unpinnedBishops);
+            uint64_t pieceIndex = _tzcnt_u64(unpinnedBishops);
+            Squares atk = slide(bishopMoves[pieceIndex], occ, pieceIndex) & notselfCheckmask;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, BISHOP>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, BISHOP>(_blsi_u64(temp) | current)); }
+        }
+    }
+
+    // queen moves
+    {
+        for (Squares queensHV = self.q & res.pinHV; queensHV; queensHV = _blsr_u64(queensHV)) {
+            Squares current = _blsi_u64(queensHV);
+            uint64_t pieceIndex = _tzcnt_u64(queensHV);
+            Squares atk = slide(rookMoves[pieceIndex], occ, pieceIndex) & notselfCheckmask & res.pinHV;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, QUEEN>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, QUEEN>(_blsi_u64(temp) | current)); }
+        }
+        for (Squares queensD = self.q & res.pinD; queensD; queensD = _blsr_u64(queensD)) {
+            Squares current = _blsi_u64(queensD);
+            uint64_t pieceIndex = _tzcnt_u64(queensD);
+            Squares atk = slide(bishopMoves[pieceIndex], occ, pieceIndex) & notselfCheckmask & res.pinHV;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, QUEEN>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, QUEEN>(_blsi_u64(temp) | current)); }
+        }
+        for (Squares unpinnedQueens = self.q & notpin; unpinnedQueens; unpinnedQueens = _blsr_u64(unpinnedQueens)) {
+            Squares current = _blsi_u64(unpinnedQueens);
+            uint64_t pieceIndex = _tzcnt_u64(unpinnedQueens);
+            Squares atk = slide(rookMoves[pieceIndex], occ, pieceIndex) & slide(bishopMoves[pieceIndex], occ, pieceIndex);
+            atk &= notselfCheckmask;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, QUEEN>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, QUEEN>(_blsi_u64(temp) | current)); }
+        }
+    }
+
+    // knight moves
+    {
+        for (Squares unpinnedKnight = self.n & notpin; unpinnedKnight; unpinnedKnight = _blsr_u64(unpinnedKnight)) {
+            Squares current = _blsi_u64(unpinnedKnight);
+            uint64_t pieceIndex = _tzcnt_u64(unpinnedKnight);
+            Squares atk = knightMoves[pieceIndex] & notselfCheckmask;
+
+            BitLoop(atk & notEnemy) { out.push_back(board.pieceMove<state, KNIGHT>(_blsi_u64(temp) | current)); }
+            BitLoop(atk & res.enemyOcc) { out.push_back(board.pieceMoveCapture<state, KNIGHT>(_blsi_u64(temp) | current)); }
+        }
+    }
+
+    // pawns
+    {
+        Squares pawnAdvance;
+        if constexpr (state.isWhite) pawnAdvance = self.p & ~wPawnLast & ~(occ << 8);
+        else pawnAdvance = self.p & ~bPawnLast & ~(occ >> 8);
+
+        Squares pawnPush;
+        if constexpr (state.isWhite) pawnPush = pawnAdvance & wPawnStart & ~(occ << 16);
+        else pawnPush = pawnAdvance & wPawnStart & ~(occ >> 16);
+
+        Squares pawnCaptureL;
+        if constexpr (state.isWhite) pawnCaptureL = self.p & ~wPawnLast & ((res.enemyOcc & notHfile) << 7);
+        else pawnCaptureL = self.p & ~wPawnLast & ((res.enemyOcc & notHfile) >> 7);
+
+        Squares pawnCaptureR;
+        if constexpr (state.isWhite) pawnCaptureR = self.p & ~wPawnLast & ((res.enemyOcc & notAfile) << 9);
+        else pawnCaptureR = self.p & ~wPawnLast & ((res.enemyOcc & notAfile) >> 9);
+
+        Squares pawnAdvancePromote;
+        if constexpr (state.isWhite) pawnAdvancePromote = self.p & wPawnLast & ~(occ << 8);
+        else pawnAdvancePromote = self.p & bPawnLast & ~(occ >> 8);
+
+        Squares pawnCaptureLpromote;
+        if constexpr (state.isWhite) pawnCaptureLpromote = self.p & wPawnLast & ((res.enemyOcc & notHfile) << 7);
+        else pawnCaptureLpromote = self.p & wPawnLast & ((res.enemyOcc & notHfile) >> 7);
+
+        Squares pawnCaptureRpromote;
+        if constexpr (state.isWhite) pawnCaptureRpromote = self.p & wPawnLast & ((res.enemyOcc & notAfile) << 9);
+        else pawnCaptureRpromote = self.p & wPawnLast & ((res.enemyOcc & notAfile) >> 9);
+
+        // not pinned
+        BitLoop(pawnAdvance & notpin) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 8;
+            else final = current << 8;            
+            out.push_back(board.pieceMove<state, PAWN>(current | final)); 
+        }
+        BitLoop(pawnPush & notpin) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 16;
+            else final = current << 16;
+            out.push_back(board.pawnPush<state>(current, current | final)); 
+        }
+        BitLoop(pawnCaptureL & notpin) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 7;
+            else final = current << 7;
+            out.push_back(board.pieceMoveCapture<state, PAWN>(current | final)); 
+        }
+        BitLoop(pawnCaptureR & notpin) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 9;
+            else final = current << 9;
+            out.push_back(board.pieceMoveCapture<state, PAWN>(current | final)); 
+        }
+        // pinned
+        BitLoop(pawnAdvance & res.pinHV) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 8;
+            else final = current << 8;            
+            if (final & res.pinHV) out.push_back(board.pieceMove<state, PAWN>(current | final)); 
+        }
+        BitLoop(pawnPush & res.pinHV) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 16;
+            else final = current << 16;
+            if (final & res.pinHV) out.push_back(board.pawnPush<state>(current, current | final)); 
+        }
+        BitLoop(pawnCaptureL & res.pinD) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 7;
+            else final = current << 7;
+            if (final & res.pindD) out.push_back(board.pieceMoveCapture<state, PAWN>(current | final)); 
+        }
+        BitLoop(pawnCaptureR & res.pinD) { 
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 9;
+            else final = current << 9;
+            if (final & res.pindD) out.push_back(board.pieceMoveCapture<state, PAWN>(current | final)); 
+        }
+        // not pinned promotion
+        BitLoop(pawnAdvancePromote & notpin) {
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 8;
+            else final = current << 8;
+            out.push_back(board.pawnPromote<state, QUEEN>(current, final));
+            out.push_back(board.pawnPromote<state, ROOK>(current, final)); 
+            out.push_back(board.pawnPromote<state, BISHOP>(current, final));
+            out.push_back(board.pawnPromote<state, KNIGHT>(current, final));
+        }
+        BitLoop(pawnCaptureLpromote & notpin) {
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 7;
+            else final = current << 7;
+            out.push_back(board.pawnPromote<state, QUEEN>(current, final));
+            out.push_back(board.pawnPromote<state, ROOK>(current, final)); 
+            out.push_back(board.pawnPromote<state, BISHOP>(current, final));
+            out.push_back(board.pawnPromote<state, KNIGHT>(current, final));
+        }
+        BitLoop(pawnCaptureRpromote & notpin) {
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 9;
+            else final = current << 9;
+            out.push_back(board.pawnPromote<state, QUEEN>(current, final));
+            out.push_back(board.pawnPromote<state, ROOK>(current, final)); 
+            out.push_back(board.pawnPromote<state, BISHOP>(current, final));
+            out.push_back(board.pawnPromote<state, KNIGHT>(current, final));
+        }
+        // pinned promotion
+        BitLoop(pawnAdvancePromote & res.pinHV) {
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 8;
+            else final = current << 8;
+            if (final & res.pinHV) {
+                out.push_back(board.pawnPromote<state, QUEEN>(current, final));
+                out.push_back(board.pawnPromote<state, ROOK>(current, final)); 
+                out.push_back(board.pawnPromote<state, BISHOP>(current, final));
+                out.push_back(board.pawnPromote<state, KNIGHT>(current, final));
+            }
+        }
+        BitLoop(pawnCaptureLpromote & res.pinD) {
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 7;
+            else final = current << 7;
+            if (final & res.pinHV) {
+                out.push_back(board.pawnPromote<state, QUEEN>(current, final));
+                out.push_back(board.pawnPromote<state, ROOK>(current, final)); 
+                out.push_back(board.pawnPromote<state, BISHOP>(current, final));
+                out.push_back(board.pawnPromote<state, KNIGHT>(current, final));
+            }
+        }
+        BitLoop(pawnCaptureRpromote & res.pinD) {
+            Squares current = _blsi_u64(temp); Squares final;
+            if constexpr (state.isWhite) final = current >> 9;
+            else final = current << 9;
+            if (final & res.pinD) {
+                out.push_back(board.pawnPromote<state, QUEEN>(current, final));
+                out.push_back(board.pawnPromote<state, ROOK>(current, final)); 
+                out.push_back(board.pawnPromote<state, BISHOP>(current, final));
+                out.push_back(board.pawnPromote<state, KNIGHT>(current, final));
+            }
+        }
+    }
+
+    // enpassant
+    if constexpr (state.ep) {
+        Squares epL = board.ep & (self.p >> 1) & ~res.epPin;
+        Squares epR = board.ep & (self.p << 1) & ~res.epPin;
+        
+        Squares enemyPawnBehind;
+        if constexpr (state.isWhite) enemyPawnBehind = epL >> 8;
+        else enemyPawnBehind = epL << 8;
+
+        if (epL) {
+            out.push_back(board.pieceMoveCapture<state, PAWN>(board.ep | epL | enemyPawnBehind));
+        }
+        if (epR) {
+            out.push_back(board.pieceMoveCapture<state, PAWN>(board.ep | epR | enemyPawnBehind));
+        }
+    }
+
+    // castles
+    if constexpr (state.isWhite) {
+        if constexpr (state.wL) {
+            if (res.checkCount == 0 && !(res.enemySeen & wLCastleSeen)) {
+                out.push_back(board.castleL<state>());
+            }
+        }
+        if constexpr (state.wR) {
+            if (res.checkCount == 0 && !(res.enemySeen & wRCastleSeen)) {
+                out.push_back(board.castleR<state>());
+            }
+        }
+    } else {
+        if constexpr (state.bL) {
+            if (res.checkCount == 0 && !(res.enemySeen & bLCastleSeen)) {
+                out.push_back(board.castleL<state>());
+            }
+        }
+        if constexpr (state.bR) {
+            if (res.checkCount == 0 && !(res.enemySeen & bRCastleSeen)) {
+                out.push_back(board.castleR<state>());
+            }
+        }
+    }
+
+    return out;
 }
